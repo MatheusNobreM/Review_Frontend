6be84a35def4596a4704c16f8358ff0a81fc315c
Matheus Moreira <matheusipsnobre@gmail.com>
2026-09-08 11:35:18 -0300

    first commit

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import Tasks fomr "./components/Tasks";
+
+function App() {
+  return (
+    <div>
+      <h1>Gerenciador de Tarefas</h1>
+      <Tasks />
+    </div>
+  );
+}
+
+export default App;
